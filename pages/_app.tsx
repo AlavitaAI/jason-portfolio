2e30ee18d88ad1a607cdbfca554788de08071ebd
Jason Conroy <alavitaapp@gmail.com>
2026-08-14 16:04:21 -0400

    Hide main website layout headers and footers on draft page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
-// pages/_app.tsx
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  if (router.pathname === "/draft") {
+    return <Component {...pageProps} />;
+  }
+
   return (
     <Layout>
       <Component {...pageProps} />
